a3b6ddb6ee6a2114b6968a27b69d6d3b20ffa4c8
annyhang <51306888+annyhang@users.noreply.github.com>
2020-01-19 11:31:04 -0500

    Update word.py

--- word.py
+++ word.py
@@ -31,8 +31,8 @@ def process(filename, JWT_TOKEN, LOGIN_URL, JOBS_URL):
         
     #send post request with authentification and the file
     job_id = json.loads(resp_sub_job.text)['job_id']
-    print('Status code:',resp_sub_job.status_code)
-    print('Response:',resp_sub_job.text)
+    #print('Status code:',resp_sub_job.status_code)
+    #print('Response:',resp_sub_job.text)
 
     #delay next step so that image can have time to process
     time.sleep(3)
@@ -40,7 +40,7 @@ def process(filename, JWT_TOKEN, LOGIN_URL, JOBS_URL):
     #retrieve the information from the json file and get the letter
 
     GET_JOB_URL = JOBS_URL + '/' +job_id
-    print(GET_JOB_URL)
+    #print(GET_JOB_URL)
     resp_get_job = requests.get(GET_JOB_URL,headers={'Authorization':f'Bearer {JWT_TOKEN}'})
     return wow.init(json.loads(resp_get_job.text))
 
@@ -48,5 +48,9 @@ if __name__ == '__main__':
     key, log, job = cloudAPI()
 
     word = process('a', key, log, job)
-    word += process('b', key, log, job)
-    word += process('c', key, log, job)
+    word = process('b', key, log, job)
+    word = process('c', key, log, job)
+
+
+
+
