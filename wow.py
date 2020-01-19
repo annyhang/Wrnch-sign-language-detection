993b272454c27b1aef2287e3668e8298ec28ee9a
AlisaFox <34177605+AlisaFox@users.noreply.github.com>
2020-01-19 09:57:07 -0500

    Update wow.py

--- wow.py
+++ wow.py
@@ -2,12 +2,12 @@ import math
 import json
 import sys
 
-def json_r(filename):
-   with open(filename) as f_in:
+def json_r(resp_get_job):
+   with open(resp_get_job) as f_in:
        return(json.load(f_in))
 
-def init(filename):
-    pose_estimation = json_r(filename)
+def init(resp_get_job):
+    pose_estimation = json_r(resp_get_job)
    
     hand_pose = pose_estimation['frames'][0]['persons'][0]
 
@@ -105,16 +105,18 @@ def init(filename):
         and withinoverlap(PTIPy, TTIPy) and not isverticalIndex \
         and not isverticalMiddle and not isverticalRing \
         and not isverticalPinky:
-            #return 'o'
+            
             print("Sign is o")
+            return 'o'
             quit()
         #for r, if index and middle DIPs are crossed, straight
         # rest are bent      
         elif withinoverlap (IDIPx, MDIPx) and withinoverlap (IDIPy, MDIPy)\
         and isverticalMiddle and isverticalIndex\
         and not isverticalRing and not isverticalPinky:
-            #return 'r'
-            print("Sign is r")
+           
+            print("Sign is r") 
+            return 'r'
             quit()
 
 
@@ -139,27 +141,31 @@ def init(filename):
         if isverticalMiddle and isverticalIndex\
         and isverticalPinky and isverticalRing\
         and thumb == 1:
-            #return 'b'
+            
             print("Sign is b")
+            return 'b'
             quit()
         #3 f
         elif isverticalPinky and isverticalRing\
         and isverticalMiddle and not isverticalIndex\
         and withinoverlap (TTIPy, ITIPy) and withinoverlap (TTIPx, ITIPx):
-            #return 'f'
-            print("Sign is f")
+          
+            print("Sign is f")  
+            return 'f'
             quit()
         #3 w
         elif isverticalIndex and isverticalMiddle and isverticalRing\
         and not isverticalPinky:
-            #return 'w'
-            print("Sign is w")
+           
+            print("Sign is w") 
+            return 'w'
             quit()
         #2 k
         elif isverticalIndex and isverticalMiddle and not isverticalRing\
         and not isverticalPinky and thumb ==2:
-            #return 'k'
+            
             print("Sign is k")
+            return 'k'
             quit()
         #2 u 
         # same x level for the vertical fingers
@@ -168,40 +174,43 @@ def init(filename):
         #otherwise v 
         elif isverticalIndex and isverticalMiddle and not isverticalRing\
         and not isverticalPinky and withinoverlap(math.fabs(IPIPx-MPIPx), math.fabs(ITIPx-MTIPx)):
-            #return 'u'
             print("Sign is u")
+            return 'u'           
             quit()
         #2 v
         elif isverticalIndex and isverticalMiddle and not isverticalRing \
-        and not isverticalPinky:
-            #return 'v'
+        and not isverticalPinky:      
             print("Sign is v")
+            return 'v'
             quit()
         #1 index
         elif isverticalIndex and not isverticalMiddle and not isverticalRing\
         and not isverticalPinky:
             #l
             if (TTIPx > IMCPx and TTIPx > PMCPx) or (TTIPx < IMCPx and TTIPx < PMCPx):
-                #return 'l'
                 print("Sign is l")
+                return 'l'                
                 quit()
             # d, z
             else:
-                #return 'd'
-                print("Sign is d or z")
+               
+                print("Sign is d or z") 
+                return 'd'
                 quit()
         #1 pinky
         elif isverticalPinky and not isverticalMiddle and not isverticalRing\
         and not isverticalIndex:
             #y
             if (TTIPx > IMCPx and TTIPx > PMCPx) or (TTIPx < IMCPx and TTIPx < PMCPx):
-                #return 'y'
+                
                 print("Sign is y")
+                return 'y'
                 quit()
             #i, j
             else:
-                #return 'i'
+                
                 print("Sign is i or j")
+                return 'i'
                 quit()
 
     # horizontal straight g or h or p
@@ -214,23 +223,27 @@ def init(filename):
             if ((MTIPx > MDIPx and MTIPx > MPIPx and MDIPx > MPIPx ) or (MTIPx < MDIPx and MTIPx < MPIPx and MDIPx < MPIPx)\
             and not withinoverlap(MTIPx, MDIPx)):
                 print (isverticalIndex)
-                #return 'h'
+                
                 print("Sign is h")
+                return 'h'
                 quit()
             # p 
             # middle finger points down
             elif MPIPy < MDIPy and MDIPy < MTIPy:
-                #return p
+                
                 print("Sign is p") 
+                return p
                 quit()
             else:
-                #return 'g'
-                print("Sign is g")
+               
+                print("Sign is g") 
+                return 'g'
                 quit()
         #q
         elif IPIPy < IDIPy and IDIPy < ITIPy and TTIPy > TDIPy:
-            #return q
+            
             print("Sign is q")
+            return q
             quit()
 
 
@@ -247,44 +260,52 @@ def init(filename):
         #t
         if (TTIPx < IPIPx and TTIPx > MPIPx) or (TTIPx > IPIPx and TTIPx < MPIPx) \
         and TTIPy < PPIPy:
-            # return t
+            
             print("Sign is t")
+             return t
             quit()
         #c
         elif ITIPy < IMCPy and MTIPy < MMCPy and RTIPy < RMCPy and PTIPy < PMCPy \
         and TTIPy > IPIPy:
-            #return c
+            
             print("Sign is c")
+            return c
             quit()
         #x
         elif ITIPy < MPIPy:
-            #return x
+            
             print ("Sign is x")
+            return x
             quit()
         #n
         elif ITIPy < RDIPy and MTIPy < RDIPy:
-            # return n
+            
             print ("Sign is n")
+            return n
             quit()
         #m
         elif ITIPy < PDIPy and MTIPy < PDIPy and RTIPy < PDIPy :
-            # return m
+            
             print ("Sign is m")    
+            return m
             quit()
         #a 
         elif (thumb==2 )and TTIPy < IPIPy:
-            #return a
-            print ("Sign is a")
+           
+            print ("Sign is a") 
+            return a
             quit()
         #e
         elif ITIPy < TTIPy and MTIPy < TTIPy:
-            #return e
+            
             print ("Sign is e")
+            return e
             quit()
         #s
         else:
-            #return s
+            
             print ("Sign is s")
+            return s
             quit()
 
 
