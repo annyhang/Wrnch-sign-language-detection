12b8595d7c7a29b447dffdb8b6906b1d33930f35
AlisaFox <34177605+AlisaFox@users.noreply.github.com>
2020-01-18 21:59:37 -0500

    Update wow.py

--- wow.py
+++ wow.py
@@ -42,13 +42,31 @@ PTIPx
 PTIPy
 
 
-#if finger is straight, true; else false
-istraightIndex= True if IPIPy > IDIPy and IDIPy > IPIPy else False
-istraightMiddle= True if MPIPy > MDIPy and MDIPy > MPIPy else False
-istraightRing= True if RPIPy > RDIPy and RDIPy > RPIPy else False
-istraightPointer= True if PPIPy > PDIPy and PDIPy > PPIPy else False
+istraightIndex= True if IPIPy > IDIPy and IDIPy > ITIPy else False
+istraightMiddle= True if MPIPy > MDIPy and MDIPy > MTIPy else False
+istraightRing= True if RPIPy > RDIPy and RDIPy > RTIPy else False
+istraightPointer= True if PPIPy > PDIPy and PDIPy > PTIPy else False
+
+#now for thumb:  straigh / curved / hidden
+#need scores for TDIP and TTIP
+TDIPscore
+TTIPscore
+# 0 for hidden, 1 for curved, 2 for straight
+thumb = -1
+
+def isThumb():
+if TPIPy > TDIPy and TDIPy > TTIPy:
+  # straight
+  thumb = 2
+elif TDIPscore < 0.9 and TTIPscore < 0.9:
+  #hidden
+  thumb = 0
+else:
+  #curved, not hidden
+  thumb = 1
    
 
 #for o, r
+# returns o or r or none
 def isItSuperimposed():
-  print("Hello from a function")
+  if 
